4399c1cfdd50deb8fae59c4eb0ab7f70258ca5aa
Jordan Tobin <jordantobin123@hotmail.com>
2025-09-20 21:21:39 -0700

    Added package destruction functionality

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    bool hasPackage = false; 
+    bool hasPackage = false;
+    [SerializeField] float destroyDelay = 0.2f;  
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(gameObject.name + " collided with " + collision + " oh the HUMANITY!");
+        //Debug.Log(gameObject.name + " collided with " + collision + " oh the HUMANITY!");
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Package")
+        if (collision.tag == "Package" && !hasPackage)
         {
             Debug.Log("You picked up a package.");
             hasPackage = true;
+            Destroy(collision.gameObject, destroyDelay);
+        }
+        else if (collision.tag == "Package" && hasPackage)
+        {
+            Debug.Log("You already have a package!"); 
         }
         else if (collision.tag == "Customer" && hasPackage)
         {
@@ -25,7 +31,7 @@ public class Delivery : MonoBehaviour
         }
         else if (collision.tag == "Customer" && !hasPackage)
         {
-            Debug.Log("WHERES MY DRINK!"); 
+            Debug.Log("WHERES MY DRINK!");
         }
     }
 }

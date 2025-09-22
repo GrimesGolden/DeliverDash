a8b7e5be6158e6c9a5b19b6f69206544b1d9bf85
Jordan Tobin <jordantobin123@hotmail.com>
2025-09-22 16:49:42 -0700

    Modified settings for bin/obj folder bug, added package color setting

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
     bool hasPackage = false;
-    [SerializeField] float destroyDelay = 0.2f;  
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 255);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 255); 
+    [SerializeField] float destroyDelay = 0.2f;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>(); 
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //Debug.Log(gameObject.name + " collided with " + collision + " oh the HUMANITY!");
@@ -19,15 +29,18 @@ public class Delivery : MonoBehaviour
             Debug.Log("You picked up a package.");
             hasPackage = true;
             Destroy(collision.gameObject, destroyDelay);
+
+            spriteRenderer.color = hasPackageColor; // TEST
         }
         else if (collision.tag == "Package" && hasPackage)
         {
-            Debug.Log("You already have a package!"); 
+            Debug.Log("You already have a package!");
         }
         else if (collision.tag == "Customer" && hasPackage)
         {
             Debug.Log("You delivered to a customer. CHOCOLATE I remember chocolate!");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor; // TEST
         }
         else if (collision.tag == "Customer" && !hasPackage)
         {
